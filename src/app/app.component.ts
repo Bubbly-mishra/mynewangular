600431bbf3bb586c5b0093be431f1d2c22a8794a
doyelmishra <doyel@dbsgroup.net>
2022-03-31 18:37:40 +0530

    modification

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,7 +27,27 @@ export class AppComponent implements OnInit{
       alert(error.message);
     }
     );
-    
+  }
+
+  public onOpenModal(artist: Artist, mode: string): void{
+
+
+    const container = document.getElementById('main-container');
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+    if(mode === 'add'){
+      button.setAttribute('data-target', 'addArtistModal');
+    }
+    if(mode === 'edit'){
+      button.setAttribute('data-target', 'editArtistModal');
+    }
+    if(mode === 'delete'){
+      button.setAttribute('data-target', 'deleteArtistModal');
+    }
+    container?.appendChild(button);
+    button.click();
   }
 
 }
